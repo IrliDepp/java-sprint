3b5acd2323b64c3aba225d99c63bee3e5a4f085e
irlimontes <irli.montes@gmail.com>
2019-03-19 20:11:29 -0600

    Commit unneeded dependencies and change applications proppertiets file name

--- src/main/java/com/desarrolladorslp/workshops/springbootjava/SpringbootJavaApplication.java
+++ src/main/java/com/desarrolladorslp/workshops/springbootjava/SpringbootJavaApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootJavaApplication.class, args);
+
+	    SpringApplication.run(SpringbootJavaApplication.class, args);
 	}
 
 }
